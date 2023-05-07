43365b7b42756270578470cec826e00faa533168
Orhan <orhnari@gmail.com>
2023-05-07 23:19:18 +0100

    third day

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import conn from "./db.js"
-
+import conn from "./db.js";
+import pageRoute from   './routes/pageRoute.js'
 
 dotenv.config()
 
@@ -17,13 +17,9 @@ app.set('view engine', 'ejs');
 //static files middleware
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.render('index');
-})
+//routes 
+app.use("/", pageRoute);
 
-app.get('/about', (req, res) => {
-    res.render('about');
-})
 
 app.listen(port, ()=> {
     console.log(`Application running on port ${port}`);
