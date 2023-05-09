4348dd54ce3ad0eddbce3b5fc983c0f1bc6f141f
Orhan <orhnari@gmail.com>
2023-05-09 15:57:50 +0100

    4th day

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import conn from "./db.js";
 import pageRoute from   './routes/pageRoute.js'
+import photoRoute from './routes/photoRoute.js'
 
 dotenv.config()
 
@@ -16,9 +17,11 @@ app.set('view engine', 'ejs');
 
 //static files middleware
 app.use(express.static('public'));
+app.use(express.json());
 
 //routes 
 app.use("/", pageRoute);
+app.use('/photos', photoRoute);
 
 
 app.listen(port, ()=> {
